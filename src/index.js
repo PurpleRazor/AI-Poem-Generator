ca392f63e98bfec115e9052e89a0a41917111d6b
JennyStyles <97371372+PurpleRazor@users.noreply.github.com>
2024-02-03 23:15:39 +0000

    Basic JS for form handling and typewriter plug in
    bug fix, delete to be removed.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings: ["Shall i compare thee to a summers day.. . "],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
