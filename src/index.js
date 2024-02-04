ad877f1dcc060998caa76c3ec732e0461e362889
JennyStyles <97371372+PurpleRazor@users.noreply.github.com>
2024-02-04 23:56:47 +0000

    API Intergration
    including some CCS tweaks and adding axios.
    Project now calls API and displays the result.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: ["Shall i compare thee to a summers day.. . "],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = `a5dd6f056bd100c144634b3tof095574`;
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context = `You are a romantic and creative AI. It is your mission to write a four line poem for the user, in basic HTML, separating each line with a <br />, using the prompt as your inspiration. At the end of the poem, sign your poem "The AI Poet", put your signature inside a <stong> element, `;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
+
+  let poemElement = document.querySelector("#poem");
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
