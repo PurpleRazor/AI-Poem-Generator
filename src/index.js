4208cff042384d5057f4c3e1ddb2929c2c8bb53c
JennyStyles <97371372+PurpleRazor@users.noreply.github.com>
2024-02-05 21:51:25 +0000

    Improved the user experience
    -Hidded poem box
    -blinks to indicate poem being generated
    -added examples for the user

--- src/index.js
+++ src/index.js
@@ -13,17 +13,18 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = `a5dd6f056bd100c144634b3tof095574`;
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
-  let context = `You are a romantic and creative AI. It is your mission to write a four line poem for the user, in basic HTML, separating each line with a <br />, using the prompt as your inspiration. At the end of the poem, sign your poem "The AI Poet", put your signature inside a <stong> element, `;
-
+  let context = `You are a romantic AI. It is your mission to write a four line poem for the user, separating each line with a <br />. Use the prompt as your inspiration. At the end of the poem, sign your poem "The AI Poet", put your signature "The AI Poet" inside a <stong> element.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<span class = "blink">⏳ Generating a poem for you about ${instructionsInput.value}.</span>`;
 
   console.log("generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
 
   axios.get(apiUrl).then(displayPoem);
-
-  let poemElement = document.querySelector("#poem");
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
